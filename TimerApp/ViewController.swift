7c5f76ef7cdefa531bcbb08cef629d50e769ded3
maniferrari <maniferrari@gmail.com>
2016-05-25 08:55:26 -0700

    test

--- TimerApp/ViewController.swift
+++ TimerApp/ViewController.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = NSTimer()
+    var time = 0
 
+    @IBOutlet var timerLabel: UILabel!
+    
+    @IBAction func play(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
+    }
+    
+    func increaseTimer() {
+        time = time+1
+        timerLabel.text = String(time)
+    }
+    
+    @IBAction func stop(sender: AnyObject) {
+        timer.invalidate()
+        time = 0
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        timer.invalidate()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
